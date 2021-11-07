a845d6690009fee56f2b795f0fb1ed0321a2eb09
phaggi <phaggi@gmail.com>
2021-11-07 18:53:18 +0300

    add rubricator to models.py

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -13,3 +13,12 @@ class Bd(models.Model):
         verbose_name_plural = 'Объявления'
         verbose_name = 'Объявление'
         ordering = ['-published']
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name']
