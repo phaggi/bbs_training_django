1cc3fcf53a1090e58d9cd9fd43aec7b699c6577c
phaggi <phaggi@gmail.com>
2021-11-07 18:04:49 +0300

    change columns for list of bulletins in admin.py

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Bd
-admin.site.register(Bd)
+
+
 # Register your models here.
+
+class BdAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'content', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content')
+
+
+admin.site.register(Bd, BdAdmin)
