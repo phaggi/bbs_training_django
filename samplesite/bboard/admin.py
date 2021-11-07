17229246457b4b7ff62b7d6ab05db243dd09bb0b
phaggi <phaggi@gmail.com>
2021-11-07 19:24:44 +0300

    add field 'rubric' to admin.py BdAdmin list_display

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from .models import Bd
+from .models import Rubric
 
 
 # Register your models here.
 
 class BdAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'content', 'published')
+    list_display = ('title', 'price', 'content', 'published', 'rubric')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content')
 
 
 admin.site.register(Bd, BdAdmin)
+admin.site.register(Rubric)
