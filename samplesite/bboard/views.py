8737545955869bbfc41537c21e5b55f32b069d25
phaggi <phaggi@gmail.com>
2021-11-07 16:30:49 +0300

    change view.py for high level command for rendering

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,5 +1,20 @@
+"""
+#  low level render
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Bd
 
 
 def index(request):
-    return HttpResponse('Здесь будет выведен список объявлений.')
+    template = loader.get_template('bboard/index.html')
+    bbs = Bd.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))
+"""
+from django.shortcuts import render
+from .models import Bd
+
+
+def index(request):
+    bbs = Bd.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
